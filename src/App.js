463eb2b26be8e83680bd7780c5de79b28d515f29
Chloé R <clositi@live.fr>
2022-02-24 13:45:46 +0100

    created input to display date picker in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import DatePicker from "./components/DatePicker";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	const [modal, setModal] = useState(false);
+
+	const handleModal = () => {
+		setModal(!modal);
+	};
+
+	return (
+		<div className="App">
+			<div className="datePicker__container">
+				<div className="datePicker__input">
+					<label htmlFor="date">Date</label>
+					<input id="date" type="text" onClick={handleModal} />
+				</div>
+				{modal ? <DatePicker /> : ""}
+			</div>
+		</div>
+	);
 }
 
 export default App;
